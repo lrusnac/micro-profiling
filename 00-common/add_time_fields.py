170063ca39eb2a7de4e9491b42309a2c0f7ae834
Leonid <leonidrusnac4@gmail.com>
2017-02-06 10:18:38 +0100

    restructure + time fields

---
+++ 00-common/add_time_fields.py
@@ -0,0 +1,38 @@
+import zipfile
+import csv
+from tqdm import tqdm
+
+from datetime import datetime
+
+PATH = '../00-common/'
+ZIPFILEPATH = 'no_serier_pruned_columns_10_percent'
+OUTFILEPATH = '10_percent_with_time_fields'
+
+csv.field_size_limit(1000000000)
+
+def get_data_file_pointer():
+    with zipfile.ZipFile(PATH + ZIPFILEPATH + '.zip') as zf:
+        r = csv.DictReader(zf.open(ZIPFILEPATH + '.csv'), delimiter=';')
+        return r
+
+
+if __name__ == '__main__':
+    fields = ['hashed_ID', 'VM_TITLE', 'VM_PRODUCTION_YEAR', 'VM_GENRE', 'VM_RUN_TIME', 'VM_RATING', 'STREAM_START_DATE', 'VOD_CATEGORY', 'VOD_CONTENT_TYPE', 'VM_IMDBID', 'HOUR_OF_DAY', 'DAY_OF_WEEK']
+
+    with open(OUTFILEPATH + '.csv', 'w') as output:
+        writer = csv.writer(output, delimiter=';')
+        writer.writerow(fields)
+        csvfile = get_data_file_pointer()
+
+        for entry in tqdm(csvfile, total=2576791):
+            date = entry['STREAM_START_DATE']
+            date = datetime.strptime(date, '%d%b%Y:%H:%M:%S.%f')
+            # date = dateutil.parser.parse(entry['STREAM_START_DATE'])
+
+            hour_of_day = date.hour
+            entry['HOUR_OF_DAY'] = hour_of_day
+
+            day_of_week = date.weekday()
+            entry['DAY_OF_WEEK'] = day_of_week
+
+            writer.writerow(map(lambda field: entry[field], fields))
