ed9a3edba80a7ba102d82b0150ed47050ec02165
Leonid <leonidrusnac4@gmail.com>
2017-02-06 14:59:14 +0100

    frequencies and output in json

--- 01-data-exploration/time-stats.py
+++ 01-data-exploration/time-stats.py
@@ -2,6 +2,7 @@ import zipfile
 import csv
 from datetime import datetime
 from tqdm import tqdm
+import json
 
 PATH = '../00-common/'
 ZIPFILEPATH = '10_percent_with_time_fields'
@@ -24,9 +25,16 @@ if __name__ == '__main__':
     csvfile = get_data_file_pointer()
 
     for entry in tqdm(csvfile, total=2576791):
-        days[entry['DAY_OF_WEEK']][entry['HOUR_OF_DAY']] = days[entry['DAY_OF_WEEK']][entry['HOUR_OF_DAY']] + 1
+        days[entry['DAY_OF_WEEK']][entry['HOUR_OF_DAY']] += 1
 
-    print days
+    for i in range(7):
+        # frequencies by day
+        day_views = sum(days[str(i)].values())
+        for j in range(24):
+            days[str(i)][str(j)] /= float(day_views)
+
+    print json.dumps(days)
+    # print days
 
     # {'20': 323216, '21': 279446, '22': 159713, '23': 88025, '1': 21988, '0': 29710, '3': 9157, '2': 12610, '5': 15679, '4': 10109, '7': 42647, '6': 27594, '9': 65360, '8': 55109, '11': 82487, '10': 75015, '13': 103162, '12': 93905, '15': 126447, '14': 114704, '17': 172888, '16': 142567, '19': 294087, '18': 231166}
     # {'1': 262546, '0': 295813, '3': 274859, '2': 259510, '5': 569395, '4': 405286, '6': 509382}
