cfde738659c824bfe7fa9452e9bf44bd461f1ed3
Casper Sørensen <cas0493@gmail.com>
2017-02-07 15:44:12 +0100

    Moved into genre model folder

---
+++ 03-genre-model/genre_frequencies.py
@@ -0,0 +1,50 @@
+import zipfile
+import csv
+import os
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+PATH = '../00-common/'
+ZIPFILEPATH = 'no_serier_pruned_columns'
+
+csv.field_size_limit(1000000000)
+frequencies = {}
+counters = {}
+
+def get_data_file_pointer():
+    with zipfile.ZipFile(PATH + ZIPFILEPATH + '.zip') as zf:
+        r = csv.DictReader(zf.open(ZIPFILEPATH + '.csv'), delimiter=';')
+        return r
+
+def addEntryGenre(genre, movie):
+    if genre not in frequencies:
+        frequencies[genre] = {}
+
+    if movie not in frequencies[genre]:
+        frequencies[genre][movie] = 0
+
+    frequencies[genre][movie] = frequencies[genre][movie] + 1
+
+    if genre not in counters:
+        counters[genre] = 0
+
+    counters[genre] = counters[genre] + 1
+
+if __name__ == '__main__':
+    csvfile = get_data_file_pointer()
+    for entry in csvfile:
+        genres = entry['VM_GENRE'].split(',')
+        if genres[0] == '':
+            addEntryGenre('Unknown', entry['VM_TITLE'])
+        else:
+            for genre in genres:
+                addEntryGenre(genre, entry['VM_TITLE'])
+
+    for genre, genre_values in frequencies.iteritems():
+        for w in sorted(genre_values, key=genre_values.get, reverse=True):
+            f = genre_values[w]/float(counters[genre])
+            frequencies[genre][w] = f
+
+    print counters
---
+++ 03-genre-model/user_genre_frequencies.py
@@ -0,0 +1,54 @@
+import zipfile
+import csv
+import os
+import math
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+PATH = '../00-common/'
+ZIPFILEPATH = 'no_serier_pruned_columns_10_percent'
+
+csv.field_size_limit(1000000000)
+frequencies = {}
+ph_table = {}
+counters = {}
+
+def get_data_file_pointer():
+    with zipfile.ZipFile(PATH + ZIPFILEPATH + '.zip') as zf:
+        r = csv.DictReader(zf.open(ZIPFILEPATH+'.csv'), delimiter=';')
+        return r
+
+def addEntryGenre(user, genre):
+    if user not in frequencies:
+        frequencies[user] = {}
+        ph_table[user] = {}
+
+    if genre not in frequencies[user]:
+        frequencies[user][genre] = 0
+
+    frequencies[user][genre] = frequencies[user][genre] + 1
+
+if __name__ == '__main__':
+    csvfile = get_data_file_pointer()
+    for entry in csvfile:
+        user = entry['hashed_ID']
+        genres = entry['VM_GENRE'].split(',')
+        if genres[0] == '':
+            addEntryGenre(user, 'Unknown')
+        else:
+            for genre in genres:
+                addEntryGenre(user, genre)
+
+        # Increment once for each entry
+        if user not in counters:
+            counters[user] = 0
+        counters[user] = counters[user] + 1
+
+    for user, genres in frequencies.iteritems():
+        for g in sorted(genres, key=genres.get, reverse=True):
+            f = genres[g]/float(counters[user])
+            ph_table[user][g] = (f, -f * math.log(f, 2))
+        # Print user's sum of entropies for his genres
+        print user + ' ' + str(sum([v[1] for v in ph_table[user].values()]))
