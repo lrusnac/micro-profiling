a0bd30470e750de8e6945753943b2484849ca422
Casper Sørensen <cas0493@gmail.com>
2017-02-09 19:40:21 +0100

    Changed common data file loader to accept only a path string instead of startup arguments. Cross-validation added for genre-model

--- 00-common/common.py
+++ 00-common/common.py
@@ -4,8 +4,6 @@ import os
 import sys
 import __main__
 
-DEFAULT_NAME = '../00-common/10_percent_with_time_fields_clean_genres'
-
 csv.field_size_limit(1000000000)
 
 def get_zip_data_file_pointer(zipPath, csvName):
@@ -18,22 +16,25 @@ def get_csv_data_file_pointer(csvPath):
     r = csv.DictReader(f, delimiter=';')
     return r
 
-def get_data_file_pointer(args):
+def get_data_file_pointer(data_file, do_print = False):
+    # Only supply a path string
+    if not isinstance(data_file, basestring):
+        raise ValueError("specified data_file not of type 'string'")
+    if do_print:
+        print_stats(data_file)
+
+    if data_file.endswith('.zip'):
+        csvName = data_file.split('/')[-1][0:-4] + '.csv'
+        return get_zip_data_file_pointer(data_file, csvName)
+    elif data_file.endswith('.csv'):
+        return get_csv_data_file_pointer(data_file)
+    else:
+        return None
+
+def print_stats(data_file_path):
     print '### STATS ###'
-    currScript = os.path.realpath(__main__.__file__)
-    currScript = currScript[currScript.index('micro-profiling') - len(currScript):]
-    print 'Script: ' + currScript
-    print 'Data: ' + (DEFAULT_NAME.split('/')[-1] if len(args) < 2 else args[1])
+    curr_script = os.path.realpath(__main__.__file__)
+    curr_script = curr_script[curr_script.index('micro-profiling') - len(curr_script):]
+    print 'Script: ' + curr_script
+    print 'Data: ' + data_file_path
     print
-
-    # Use default data file
-    if len(args) < 2:
-        zipPath = DEFAULT_NAME + '.zip'
-        csvName = DEFAULT_NAME.split('/')[-1] + '.csv'
-        return get_zip_data_file_pointer(zipPath, csvName)
-
-    if args[1].endswith('.zip'):
-        csvName = args[1].split('/')[-1][0:-4] + '.csv'
-        return get_zip_data_file_pointer(args[1], csvName)
-    if args[1].endswith('.csv'):
-        return get_csv_data_file_pointer(args[1])
