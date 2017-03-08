13b10abd22ad45611d8d6989b77cfadc999db48f
Leonid Rusnac <leonidrusnac4@gmail.com>
2017-03-08 10:38:56 +0100

    remove the faulty graph

--- 05-clustering/cluster_tests.py
+++ 05-clustering/cluster_tests.py
@@ -1,10 +1,6 @@
 import sys
 import numpy as np
 import sklearn.cluster as cl
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import style
-style.use("ggplot")
 
 if __name__ == '__main__':
     # dataset = np.genfromtxt(sys.argv[1], delimiter=';', usecols=(2, 3, 10, 11), dtype=None, names=True, comments='@')
@@ -17,21 +13,9 @@ if __name__ == '__main__':
     print kmeans.labels_
     print kmeans.cluster_centers_
 
-    colors = 10*['r','g','b','c','k','y','m']
     labels = kmeans.labels_
     cluster_centers = kmeans.cluster_centers_
 
-    print(colors)
     print(labels)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    for i in range(len(dataset)):
-        ax.scatter(dataset[i][0], dataset[i][1], dataset[i][2], c=colors[labels[i]], marker='o')
-
-
-    ax.scatter(cluster_centers[:,0],cluster_centers[:,1],cluster_centers[:,2],
-                marker="x",color='k', s=150, linewidths = 5, zorder=10)
-
-    plt.savefig('test.png')
+    # merge dataset with labels and do the cross validation as we do with genres
