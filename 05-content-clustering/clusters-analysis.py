a02b954564ecd4ce59dae81bb0777a1d294f0a73
Leonid Rusnac <leonidrusnac4@gmail.com>
2017-03-17 10:34:43 +0100

    print before

--- 05-content-clustering/clusters-analysis.py
+++ 05-content-clustering/clusters-analysis.py
@@ -15,12 +15,14 @@ if __name__ == "__main__":
     csvfile = get_data_file_pointer(sys.argv[1])
 
     print 'Creating the hashmaps for accounts indexes'
-    for transact in tqdm(csvfile, total=1771549):
+    for transact in tqdm(csvfile, total=2576791):
         if transact['hashed_ID'] not in accounts:
 	    accounts[transact['hashed_ID']] = len(accounts)
 
         if int(transact['KMeans']) > n_clusters:
             n_clusters = int(transact['KMeans'])
+
+    print 'n_clusters' + str(n_clusters)
 
     col = []
     row = []
@@ -28,10 +30,8 @@ if __name__ == "__main__":
     csvfile = get_data_file_pointer(sys.argv[1])
 
     print 'creating the rows and cols lists'
-    for transact in tqdm(csvfile, total=1771549):
+    for transact in tqdm(csvfile, total=2576791):
         row.append(accounts[transact['hashed_ID']])
         col.append(int(transact['KMeans']))
 
     matr = coo_matrix((np.ones(len(row)), (np.array(row), np.array(col))), shape=(len(accounts), n_clusters))
-
-    print 'n_clusters' + str(n_clusters)
