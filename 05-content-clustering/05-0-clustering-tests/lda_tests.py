a9d776172e94d02a4db14e755afa88d07828d2cb
Casper Sørensen <cas0493@gmail.com>
2017-03-22 15:51:39 +0100

    LDA test and output

--- 05-content-clustering/05-0-clustering-tests/lda_tests.py
+++ 05-content-clustering/05-0-clustering-tests/lda_tests.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import sklearn.decomposition as dec
+from sklearn.preprocessing import normalize
 sys.path.insert(0, '../../00-common')
 from common import get_data_file_pointer
 
@@ -9,6 +10,7 @@ from scipy.sparse import coo_matrix
 
 accounts = {}
 n_clusters = -1
+min_topic_p = 0.1
 
 if __name__ == '__main__':
     # create the sparse matrix
@@ -21,6 +23,11 @@ if __name__ == '__main__':
 
         if int(transact['KMeans']) > n_clusters:
             n_clusters = int(transact['KMeans'])
+
+    # Populate inv_accounts
+    inv_accounts = [""] * len(accounts)
+    for u_hash, i in accounts.iteritems():
+        inv_accounts[i] = u_hash
 
     n_clusters += 1
     print 'n_clusters ' + str(n_clusters)
@@ -36,40 +43,52 @@ if __name__ == '__main__':
         col.append(int(transact['KMeans']))
 
     matr = coo_matrix((np.ones(len(row)), (np.array(row), np.array(col))), shape=(len(accounts), n_clusters))
+    # Normalize: 'l1' = each row sums to 1
+    matr = normalize(matr, axis=1, norm='l1')
 
     print matr.todense()
 
     lda = dec.LatentDirichletAllocation(n_topics=30, n_jobs=-1, learning_method='batch', verbose=3, max_iter=1)
 
-    print 'lda creates, starting fit'
+    print 'lda created, starting fit'
 
     lda.fit(matr)
 
     print 'fit finished'
 
-    print lda
-    print lda.components_
+    # print lda
+    # print lda.components_
 
     transformed = lda.transform(matr)
-    micro_cands = []
+    micro_cands = {}
 
-    for topics_dist in transformed:
+    for i, topics_dist in enumerate(transformed):
         good_nums = 0
         for num in topics_dist:
-            if num >= 0.1:
+            if num >= min_topic_p:
                 good_nums = good_nums + 1
         if good_nums > 1:
-            micro_cands.append(topics_dist)
-            print good_nums
+            micro_cands[inv_accounts[i]] = topics_dist
+            #print good_nums
 
     print "Accounts: {}".format(len(accounts))
     print "Micro-profile cands.: {}".format(len(micro_cands))
 
-    # model = lda
-    # feature_names = ['year', 'hour', 'day']
-    # n_top_words = 20
-    # for topic_idx, topic in enumerate(model.components_):
-    #     print("Topic #%d:" % topic_idx)
-    #     print(" ".join([feature_names[i]
-    #                     for i in topic.argsort()[:-n_top_words - 1:-1]]))
-    # print()
+    for topic in transformed.dot(normalize(lda.components_, axis=1, norm='l1')):
+        print "{} {}".format(sum(topic), topic)
+
+    for u_hash, topic_dist in micro_cands.iteritems():
+        topics = []
+        for i, p in enumerate(topic_dist):
+            if p >= min_topic_p:
+                topics.append("{:>2}: {:>6.4}".format(i, p))
+        print "{} {}".format(u_hash, topics)
+
+    #
+    
+    #print transformed.dot(normalize(lda.components_, axis=1, norm='l1'))
+
+    #print 'First candidate'
+
+
+    
