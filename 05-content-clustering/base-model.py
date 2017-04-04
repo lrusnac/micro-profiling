45d43ab69bb6320bebfeafd572a00c71de81ccd2
Leonid Rusnac <leonidrusnac4@gmail.com>
2017-04-04 13:42:56 +0200

    base model finished

--- 05-content-clustering/base-model.py
+++ 05-content-clustering/base-model.py
@@ -5,6 +5,7 @@ from common import get_data_file_pointer
 from common import get_line_count
 
 import csv
+import math
 from tqdm import tqdm
 
 field_name = 'KMeans'
@@ -20,4 +21,20 @@ if __name__ == '__main__':
             clusters[entry[field_name]] = 0
         clusters[entry[field_name]] += 1
 
-    print {k:v/float(lines) for k,v in clusters.items()}
+    clusters = {k:v/float(lines) for k,v in clusters.items()}
+
+    print clusters
+    print len(clusters)
+
+    csvfile = get_data_file_pointer(sys.argv[2])
+    accuracy = 0
+    loss = 0
+    test_lines = get_line_count(sys.argv[2])
+    for entry in tqdm(csvfile, total=test_lines):
+        if entry[field_name] in clusters:
+            accuracy -= math.log(clusters[entry[field_name]])
+        else:
+            loss += 1
+
+    print 'entropy: {}'.format(accuracy / float(test_lines-loss))
+    print 'loss: {}'.format(loss)
