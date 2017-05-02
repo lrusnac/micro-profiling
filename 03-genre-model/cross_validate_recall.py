fcf3ad5e82c907b676916439d634591f70d94823
Leonid Rusnac <leonidrusnac4@gmail.com>
2017-05-02 12:01:14 +0200

    user-genre recall metric

---
+++ 03-genre-model/cross_validate_recall.py
@@ -0,0 +1,115 @@
+import sys
+sys.path.insert(0, '../00-common')
+
+from common import get_data_file_pointer
+from common import get_line_count
+
+from tqdm import tqdm
+import numpy
+
+user_genre = {}
+user_genre_ph_table = {}
+internal_user_counters = {}
+
+genre_movie = {}
+genre_movie_ph_table = {}
+internal_genre_counter = {}
+
+def compute_recall(account, k=20):
+    # accounts ==== all the transactions of one user, must not be empty
+    customer = account[0]['hashed_ID']
+    relevant_docs = set(map(lambda x: x['VM_TITLE'], account))
+
+    k_movie_set = set(makeUserPredictions(customer)[:k])
+    # make k predictions for each account
+    #   |relevant docs intersect retrieved docs| / |relevant docs|
+
+    return len(relevant_docs & k_movie_set) / float(len(relevant_docs))
+
+def metric_agregator(account):
+    return (compute_recall(account, 10), compute_recall(account, 20), compute_recall(account, 50))
+
+def metrics_evaluater(filepath, metrics):
+    account = []
+    previous_customer = ''
+
+    result_list = []
+
+    csvfile = get_data_file_pointer(filepath)
+    for entry in tqdm(csvfile, total=get_line_count(filepath)):
+        if previous_customer is not '' and entry['hashed_ID'] == previous_customer:
+            account.append(entry)
+        else:
+            # new customer
+            if previous_customer is not '':
+                result_list.append(metrics(account))
+            previous_customer = entry['hashed_ID']
+            account = []
+            account.append(entry)
+    result_list.append(metrics(account))
+    return result_list
+
+def addEntryGenre(user, genre):
+    if user not in user_genre:
+        user_genre[user] = {}
+        user_genre_ph_table[user] = {}
+
+    if genre not in user_genre[user]:
+        user_genre[user][genre] = 0
+
+    user_genre[user][genre] = user_genre[user][genre] + 1
+
+    if user not in internal_user_counters:
+        internal_user_counters[user] = 0
+    internal_user_counters[user] = internal_user_counters[user] + 1
+
+def addEntryMovie(movie, genre):
+    if genre not in genre_movie:
+        genre_movie[genre] = {}
+        genre_movie_ph_table[genre] = {}
+
+    if movie not in genre_movie[genre]:
+        genre_movie[genre][movie] = 0
+
+    genre_movie[genre][movie] = genre_movie[genre][movie] + 1
+
+    if genre not in internal_genre_counter:
+        internal_genre_counter[genre] = 0
+
+    internal_genre_counter[genre] = internal_genre_counter[genre] + 1
+
+def makeUserPredictions(user):
+    predictions = {}
+    for genre_set, p_i in user_genre_ph_table[user].iteritems:
+        for movie, q_x in genre_movie_ph_table[genre_set].iteritems:
+            # check if movie appears in several genre sets?
+            predictions[movie] = p_i * q_x
+    return sorted(predictions, key=predictions.get, reverse=True)
+
+def build_model(train_file):
+    trainset = get_data_file_pointer(train_file, True)
+    for entry in tqdm(trainset, total=get_line_count(train_file)):
+        user = entry['hashed_ID']
+        addEntryGenre(user, entry['VM_GENRE'])
+        addEntryMovie(entry['VM_TITLE'], entry['VM_GENRE'])
+
+    # compute the user_genre_ph_table
+    for user, genres in user_genre.iteritems():
+        for g in genres:
+            f = genres[g]/float(internal_user_counters[user])
+            user_genre_ph_table[user][g] = f
+
+    # compute the genre_movie_ph_table
+    for genre, genre_values in genre_movie.iteritems():
+        for w in genre_values:
+            f = genre_values[w]/float(internal_genre_counter[genre])
+            genre_movie_ph_table[genre][w] = f
+
+if __name__ == '__main__':
+    train_file = sys.argv[1]
+    test_file = sys.argv[2]
+
+    build_model(train_file)
+
+    results = metrics_evaluater(test_file, metric_agregator)
+    print [sum(y) / len(y) for y in zip(*results)]
