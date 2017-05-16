696f01de2a28120858d685b5d309c6e3f544b0f7
Leonid Rusnac <leonidrusnac4@gmail.com>
2017-05-16 15:06:18 +0200

    run everything 10 times

--- run_recall.sh
+++ run_recall.sh
@@ -12,7 +12,7 @@ test=$fulldatasetnofilename'_test.csv'
 
 cd 00-common
 
-for i in `seq 1 1`;
+for i in `seq 1 10`;
 do
     echo '== ITERATION: '$i ' =='
     echo '[INFO] dividing the data into train and test datasets'
@@ -23,16 +23,16 @@ do
 
     echo '[INFO] frequency model'
     python ../02-frequency-model/freq_recall.py $trainc $test
-    # echo '[INFO] genre baseline model'
-    # python ../03-genre-model/base_model_recall.py $trainc $test
-    # echo '[INFO] personalised genre model'
-    # python ../03-genre-model/cross_validate_recall.py $trainc $test
-    # echo '[INFO] lda model'
-    # python ../06-latent-dirichlet-allocation/lda_recall.py $trainc $test
-    # echo '[INFO] clustering + lda model'
-    # python ../06-latent-dirichlet-allocation/lda_w_clustering_recall.py $trainc $test
-    # echo '[INFO] removing train and test datasets'
-    # rm $trainc
-    # rm $train
-    # rm $test
+    echo '[INFO] genre baseline model'
+    python ../03-genre-model/base_model_recall.py $trainc $test
+    echo '[INFO] personalised genre model'
+    python ../03-genre-model/cross_validate_recall.py $trainc $test
+    echo '[INFO] lda model'
+    python ../06-latent-dirichlet-allocation/lda_recall.py $trainc $test
+    echo '[INFO] clustering + lda model'
+    python ../06-latent-dirichlet-allocation/lda_w_clustering_recall.py $trainc $test
+    echo '[INFO] removing train and test datasets'
+    rm $trainc
+    rm $train
+    rm $test
 done
