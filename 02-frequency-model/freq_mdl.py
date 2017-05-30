86ffa7c24819e7c4a6bdf116136ebde5c9c9fd1d
Leonid Rusnac <leonidrusnac4@gmail.com>
2017-05-30 13:06:18 +0200

    new mdl for freq model

--- 02-frequency-model/freq_mdl.py
+++ 02-frequency-model/freq_mdl.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 counters = {}
 frequencies = []
 movie2index = {}
+c = 11.5
 
 if __name__ == '__main__':
     rows_count = 0
@@ -24,10 +25,18 @@ if __name__ == '__main__':
 
     # calculate movies' frequencies
     for i, movie in enumerate(sorted(counters, key=counters.get, reverse=True)):
-        f = counters[movie]/float(rows_count)
-        frequencies.append(f)
+        p = counters[movie]/float(rows_count)
+        p = round(c * math.log(1/p, 2), 0)
+        p = math.pow(math.pow(2, p/c), -1)
+        frequencies.append(p)
         movie2index[movie] = i
+    
+    freq2 = []
+    sum_of_p = sum(frequencies)
+    for f in frequencies:
+        freq2.append(f + (1-sum_of_p) * (f / float(sum_of_p)))
 
+    frequencies = freq2
 
     c_movies = len(frequencies)
     for i in xrange(0, int(c_movies / 100)):
@@ -42,6 +51,8 @@ if __name__ == '__main__':
             movie = entry['VM_TITLE']
             p = frequencies[movie2index[movie]]
             p = p if p >= frequencies[top_n-1] else def_val
+            if p <= 0:
+                print 'p: {}, top_n-1 {}, freq: {}, defval: {}'.format(p, top_n-1, frequencies[top_n-1], def_val)
             guess_accuracy_sum -= math.log(p, 2)
             entry_count += 1
 
